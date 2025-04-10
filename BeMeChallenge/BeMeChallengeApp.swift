63dc95c3dfd32a62867c06f92ff00aa05e94737e
jamiebhpark <jongfur@gmail.com>
2025-04-10 16:59:54 +0900

    Initial Commit

---
+++ BeMeChallenge/BeMeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeMeChallengeApp.swift
+//  BeMeChallenge
+//
+//  Created by JONGHUN PARK on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BeMeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
